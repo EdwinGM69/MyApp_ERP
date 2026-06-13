a9997f4330c349f700ba7459c39218d4d7b71352
EdwinGM69 <egabanchom@gmail.com>
2026-06-12 22:05:58 -0500

    Fix consultas kardex, stock y caja

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -415,7 +415,7 @@ async function main() {
   await prisma.opcionMenu.upsert({
     where: { modulo_id_codigo: { modulo_id: adminModulo.id, codigo: 'ROL_USUARIO' } },
     update: {},
-    create: { modulo_id: adminModulo.id, parent_id: parentAdministracion.id, codigo: 'ROL_USUARIO', descripcion: 'Rol de Usuario', ruta: '/roles', orden: 42 activo: true, created_by: 1 },
+    create: { modulo_id: adminModulo.id, parent_id: parentAdministracion.id, codigo: 'ROL_USUARIO', descripcion: 'Rol de Usuario', ruta: '/roles', orden: 42, activo: true, created_by: 1 },
   })
 
   await prisma.opcionMenu.upsert({
